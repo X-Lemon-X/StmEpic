ba34336a57e842e6dc721dd9b1837cfe90c64194
X-Lemon-X <github-actions[bot]@users.noreply.github.com>
2025-01-16 21:43:08 +0000

    Apply formating to X-Lemon-X commit.

--- src/Algorithm/vectors3d.hpp
+++ src/Algorithm/vectors3d.hpp
@@ -3,94 +3,92 @@
 #include "stmepic.hpp"
 
 
-namespace stmepic::algorithm{
+namespace stmepic::algorithm {
 
-template <typename T>
-struct Vector3d_t{
+template <typename T> struct Vector3d_t {
   using type = T;
   static_assert(std::is_arithmetic<type>::value, "Vector3d_t<T> requires T to be an arithmetic type");
   T x;
   T y;
   T z;
 
-  Vector3d_t<T> operator+(const Vector3d_t<T>& rhs){
-    return {x + rhs.x, y + rhs.y, z + rhs.z};
+  Vector3d_t<T> operator+(const Vector3d_t<T>& rhs) {
+    return { x + rhs.x, y + rhs.y, z + rhs.z };
   }
 
-  Vector3d_t<T> operator-(const Vector3d_t<T>& rhs){
-    return {x - rhs.x, y - rhs.y, z - rhs.z};
+  Vector3d_t<T> operator-(const Vector3d_t<T>& rhs) {
+    return { x - rhs.x, y - rhs.y, z - rhs.z };
   }
 
-  Vector3d_t<T> operator*(const T& rhs){
-    return {x * rhs, y * rhs, z * rhs};
+  Vector3d_t<T> operator*(const T& rhs) {
+    return { x * rhs, y * rhs, z * rhs };
   }
 
-  Vector3d_t<T> operator/(const T& rhs){
-    return {x / rhs, y / rhs, z / rhs};
+  Vector3d_t<T> operator/(const T& rhs) {
+    return { x / rhs, y / rhs, z / rhs };
   }
 
-  Vector3d_t<T> operator+=(const Vector3d_t<T>& rhs){
+  Vector3d_t<T> operator+=(const Vector3d_t<T>& rhs) {
     x += rhs.x;
     y += rhs.y;
     z += rhs.z;
     return *this;
   }
 
-  Vector3d_t<T> operator-=(const Vector3d_t<T>& rhs){
+  Vector3d_t<T> operator-=(const Vector3d_t<T>& rhs) {
     x -= rhs.x;
     y -= rhs.y;
     z -= rhs.z;
     return *this;
   }
 
-  Vector3d_t<T> operator*=(const T& rhs){
+  Vector3d_t<T> operator*=(const T& rhs) {
     x *= rhs;
     y *= rhs;
     z *= rhs;
     return *this;
   }
 
-  Vector3d_t<T> operator/=(const T& rhs){
+  Vector3d_t<T> operator/=(const T& rhs) {
     x /= rhs;
     y /= rhs;
     z /= rhs;
     return *this;
   }
 
-  bool operator==(const Vector3d_t<T>& rhs){
+  bool operator==(const Vector3d_t<T>& rhs) {
     return x == rhs.x && y == rhs.y && z == rhs.z;
   }
 
-  bool operator!=(const Vector3d_t<T>& rhs){
+  bool operator!=(const Vector3d_t<T>& rhs) {
     return x != rhs.x || y != rhs.y || z != rhs.z;
   }
 };
 
-template <typename T>
-struct Vector4d_t{
+template <typename T> struct Vector4d_t {
   using type = T;
   T x;
   T y;
   T z;
   T w;
 
-  Vector4d_t<T> operator+(const Vector4d_t<T>& rhs){
-    return {x + rhs.x, y + rhs.y, z + rhs.z, w + rhs.w};
+  Vector4d_t<T> operator+(const Vector4d_t<T>& rhs) {
+    return { x + rhs.x, y + rhs.y, z + rhs.z, w + rhs.w };
   }
 
-  Vector4d_t<T> operator-(const Vector4d_t<T>& rhs){
-    return {x - rhs.x, y - rhs.y, z - rhs.z, w - rhs.w};
+  Vector4d_t<T> operator-(const Vector4d_t<T>& rhs) {
+    return { x - rhs.x, y - rhs.y, z - rhs.z, w - rhs.w };
   }
 
-  Vector4d_t<T> operator*(const T& rhs){
-    return {x * rhs, y * rhs, z * rhs, w * rhs};
+  Vector4d_t<T> operator*(const T& rhs) {
+    return { x * rhs, y * rhs, z * rhs, w * rhs };
   }
 
-  Vector4d_t<T> operator/(const T& rhs){
-    return {x / rhs, y / rhs, z / rhs, w / rhs};
+  Vector4d_t<T> operator/(const T& rhs) {
+    return { x / rhs, y / rhs, z / rhs, w / rhs };
   }
 
-  Vector4d_t<T> operator+=(const Vector4d_t<T>& rhs){
+  Vector4d_t<T> operator+=(const Vector4d_t<T>& rhs) {
     x += rhs.x;
     y += rhs.y;
     z += rhs.z;
@@ -98,7 +96,7 @@ struct Vector4d_t{
     return *this;
   }
 
-  Vector4d_t<T> operator-=(const Vector4d_t<T>& rhs){
+  Vector4d_t<T> operator-=(const Vector4d_t<T>& rhs) {
     x -= rhs.x;
     y -= rhs.y;
     z -= rhs.z;
@@ -106,7 +104,7 @@ struct Vector4d_t{
     return *this;
   }
 
-  Vector4d_t<T> operator*=(const T& rhs){
+  Vector4d_t<T> operator*=(const T& rhs) {
     x *= rhs;
     y *= rhs;
     z *= rhs;
@@ -114,7 +112,7 @@ struct Vector4d_t{
     return *this;
   }
 
-  Vector4d_t<T> operator/=(const T& rhs){
+  Vector4d_t<T> operator/=(const T& rhs) {
     x /= rhs;
     y /= rhs;
     z /= rhs;
@@ -122,11 +120,11 @@ struct Vector4d_t{
     return *this;
   }
 
-  bool operator==(const Vector4d_t<T>& rhs){
+  bool operator==(const Vector4d_t<T>& rhs) {
     return x == rhs.x && y == rhs.y && z == rhs.z && w == rhs.w;
   }
 
-  bool operator!=(const Vector4d_t<T>& rhs){
+  bool operator!=(const Vector4d_t<T>& rhs) {
     return x != rhs.x || y != rhs.y || z != rhs.z || w != rhs.w;
   }
 };
--- src/Sensors/BNO055.cpp
+++ src/Sensors/BNO055.cpp
@@ -1,27 +1,28 @@
 
-#include "stmepic.hpp"
-#include "pin.hpp"
-#include "device.hpp"
 #include "BNO055.hpp"
+#include "device.hpp"
+#include "pin.hpp"
+#include "stmepic.hpp"
 
 using namespace stmepic::sensors::imu;
 
 
-BNO055::BNO055(I2C_HandleTypeDef& i2c, gpio::GpioPin *reset) : i2c(i2c), reset(reset) {
+BNO055::BNO055(I2C_HandleTypeDef& i2c, gpio::GpioPin* reset) : i2c(i2c), reset(reset) {
   device_reset();
 }
 
-Status BNO055::set_page(internal::BNO055_PAGE_t page){
-  auto status = HAL_I2C_Mem_Write(&i2c, internal::BNO055_I2C_ADDRESS, internal::BNO055_REG_PAGE_ID, 1, (uint8_t*)&page, 1, 100);
+Status BNO055::set_page(internal::BNO055_PAGE_t page) {
+  auto status = HAL_I2C_Mem_Write(&i2c, internal::BNO055_I2C_ADDRESS,
+                                  internal::BNO055_REG_PAGE_ID, 1, (uint8_t*)&page, 1, 100);
   return stmepic::DeviceTranslateStatus::translate_hal_status_to_status(status);
 }
 
-Status BNO055::set_operation_mode(internal::BNO055_OPR_MODE_t mode){
+Status BNO055::set_operation_mode(internal::BNO055_OPR_MODE_t mode) {
   set_page(internal::BNO055_PAGE_t::BNO055_PAGE_0);
-  auto status = HAL_I2C_Mem_Write(&i2c, internal::BNO055_I2C_ADDRESS, internal::BNO055_REG_OPR_MODE, 1, (uint8_t*)&mode, 1, 100);
+  auto status = HAL_I2C_Mem_Write(&i2c, internal::BNO055_I2C_ADDRESS,
+                                  internal::BNO055_REG_OPR_MODE, 1, (uint8_t*)&mode, 1, 100);
   return stmepic::DeviceTranslateStatus::translate_hal_status_to_status(status);
 }
 
-Status BNO055::device_start(){
-
+Status BNO055::device_start() {
 }
