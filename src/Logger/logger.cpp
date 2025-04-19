e9db9bd5f41ec01b5a38bde6f3b978c96eea536c
Patryk Dudziński <dodpat02@gmail.com>
2025-04-20 00:57:14 +0800

    [loger] added Semihosting for loger along withoptions to enable that

--- src/Logger/logger.cpp
+++ src/Logger/logger.cpp
@@ -6,6 +6,9 @@
 
 using namespace stmepic;
 
+extern "C" void initialise_monitor_handles(void);
+
+
 Logger::Logger() {
   log_level         = LOG_LEVEL::LOG_LEVEL_WARNING;
   transmit_function = nullptr;
@@ -13,13 +16,20 @@ Logger::Logger() {
   version           = "";
 }
 
-Status Logger::init(LOG_LEVEL level, bool _print_info, transmit_data_func _transmi_function, std::string _version) {
-  if(_transmi_function == nullptr)
-    return Status::ExecutionError("Transmit function is nullptr");
+Status Logger::init(LOG_LEVEL level, bool _print_info, transmit_data_func _transmi_function, bool _use_semihosting, std::string _version) {
+  if(_transmi_function == nullptr && !_use_semihosting)
+    return Status::ExecutionError("Transmit function nullptr ");
+  if(_transmi_function != nullptr && _use_semihosting)
+    return Status::ExecutionError("Transmit function and semihosting can't be used at the same time");
   log_level         = level;
   transmit_function = _transmi_function;
   print_info        = _print_info;
   version           = _version;
+  use_semihosting   = _use_semihosting;
+  if(use_semihosting) {
+    initialise_monitor_handles();
+    transmit_function = nullptr;
+  }
   return Status::OK();
 }
 
@@ -48,11 +58,10 @@ void Logger::debug(std::string msg, const char *file, const char *function_name)
 }
 
 void Logger::transmit(std::string msg, std::string prefix, const char *file, const char *function_name) {
-  // vPortEnterCritical();
   if(print_info) {
     std::string debug_info = "";
     if(file != nullptr && function_name != nullptr)
-      debug_info = "," + key_value_to_json("file", file) + "," + key_value_to_json("function", function_name);
+      debug_info = "," + key_value_to_json("file", file) + "," + key_value_to_json("fun", function_name);
     msg = "{\"time\":\"" + std::to_string(HAL_GetTick()) + "\",\"level\":\"" + prefix + "\",\"ver\":\"" +
           version + "\"" + debug_info + ",\"msg\":{" + msg + "}}\n";
   } else {
@@ -60,7 +69,8 @@ void Logger::transmit(std::string msg, std::string prefix, const char *file, con
   }
   if(transmit_function)
     transmit_function((uint8_t *)msg.c_str(), msg.length());
-  // vPortExitCritical();
+  if(use_semihosting)
+    printf("%s", msg.c_str());
 }
 
 std::string Logger::key_value_to_json(std::string key, std::string value) {
